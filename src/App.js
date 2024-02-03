4f3f9fdfc155de26f8883f46d811dd92f5591ae4
Julia Chanas <juulia.chanas@gmail.com>
2024-02-03 15:11:47 +0100

    Add BrowserRouter, Routes, Route to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import store from './redux/store';
+import Homepage from './components/pages/Homepage/HomePage';
+import TablePage from './components/pages/TablePage/TablePage';
+import NotFound from './components/pages/NotFound/NotFound';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <h1>Hello world</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/table/:id' element={<TablePage />} />
+          <Route path='*' element={<NotFound />} />
+          {/* <h1>Hello world</h1> */}
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 };
