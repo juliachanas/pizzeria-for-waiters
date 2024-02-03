5769592bf0207182c4ffe0a8fbbda61c7b4b04f6
Julia Chanas <juulia.chanas@gmail.com>
2024-02-03 14:18:09 +0100

    Fix App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 
 const App = () => {
-  <Provider store={store}>
-    return <h1>Hello world</h1>;
-  </Provider>;
+  return (
+    <Provider store={store}>
+      <h1>Hello world</h1>
+    </Provider>
+  );
 };
 
 export default App;
