1322de951900c261429b6c4c153b5239327e16d0
Julia Chanas <juulia.chanas@gmail.com>
2024-02-03 14:23:54 +0100

    Create tablesRedyx.js with subreducer

---
+++ src/redux/tablesRedux.js
@@ -0,0 +1,15 @@
+//selectors
+
+//actions
+const createActionName = (actionName) => `app/tables/${actionName}`;
+
+//action creators
+
+const tablesReducer = (statePart = [], action) => {
+  switch (action.type) {
+    default:
+      return statePart;
+  }
+};
+
+export default tablesReducer;
