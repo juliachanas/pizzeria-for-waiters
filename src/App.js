9bf4c1527c6a5bef152cda101b103d78a6d4aeee
Julia Chanas <juulia.chanas@gmail.com>
2024-02-03 17:22:29 +0100

    Try use api server - doesnt work

--- src/App.js
+++ src/App.js
@@ -7,18 +7,36 @@ import Footer from './components/views/Footer/Footer';
 import Homepage from './components/pages/Homepage/HomePage';
 import TablePage from './components/pages/TablePage/TablePage';
 import NotFound from './components/pages/NotFound/NotFound';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchTables } from './redux/tablesRedux';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const tables = useSelector((state) => state.tables); // Pobieramy stan tabel z Redux Store
+
+  useEffect(() => {
+    // Wysyłamy żądanie pobrania tabel po zamontowaniu komponentu App
+    dispatch(fetchTables());
+  }, [dispatch]);
+
+  // Sprawdzamy, czy dane zostały już pobrane
+  const isDataLoaded = tables.length > 0;
+
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Container>
           <Header />
-          <Routes>
-            <Route path='/' element={<Homepage />} />
-            <Route path='/table/:id' element={<TablePage />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
+          {isDataLoaded ? ( // Renderujemy zawartość tylko gdy dane są załadowane
+            <Routes>
+              <Route path='/' element={<Homepage />} />
+              <Route path='/table/:id' element={<TablePage />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          ) : (
+            <p>Loading...</p> // Komunikat ładowania, można dostosować do potrzeb
+          )}
           <Footer />
         </Container>
       </BrowserRouter>
